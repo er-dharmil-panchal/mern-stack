e433c18b7e7f9e44d2a2765d0281425deefa2f5b
Dharmil <dharmil132@gmail.com>
2026-03-24 20:57:31 +0530

    Implement CRUD operations for users with JSON file persistence; add middleware for JSON parsing; enhance user feedback on creation, update, and deletion.

--- Backend-Node-Express/Projects/1_REST/index.js
+++ Backend-Node-Express/Projects/1_REST/index.js
@@ -2,19 +2,25 @@
 
 const express = require('express');
 const users = require('./MOCK_DATA.json')
+const fs = require('fs')
 
 const app = express()
 const Port = 8000
 
+
+// This is Middleware. later we will learn about it. for now it will convert the incoming data to json format and we can access it in req.body.
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // Routes
 
 // if it is /api then the server know that it is and api call so it will return json data
-app.get('/api/users',(req,res) => {
+app.get('/api/users', (req, res) => {
      return res.json(users)
 })
 
 // here we have to render a HTML page...
-app.get('/users',(req,res) => { 
+app.get('/users', (req, res) => {
      const html = `
      <ul>
           ${users.map(user => `<li> ${user.first_name} </li>`).join("")}
@@ -27,7 +33,7 @@ app.get('/users',(req,res) => {
 
 
 //dynamic path.
-app.get('/api/users/:id', (req,res) =>{
+app.get('/api/users/:id', (req, res) => {
      const id = Number(req.params.id);
      const user = users.find(user => user.id === id);
      return res.json(user);
@@ -37,35 +43,61 @@ app.get('/api/users/:id', (req,res) =>{
 // here we cant actully perform the post and patch request because we are using the json file as our database.
 // so we will just return a message that the user is created or updated.
 
-app.post('/api/users',(req,res) =>{
-     // NOTE - Create a new user
-     return res.json({"message": "User Created"})
-})
+app.post('/api/users', (req, res) => {
+     const body = req.body;
+     console.log("Received body:", body);
+     users.push({ id: users.length + 1, ...body }); // this is just for testing, in real world we will use a database and it will handle the id generation.
+     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+          return res.json({ "message": "User Created", "data": body, "id": users.length })
+     });
+});
 
-app.patch('/api/users/:id',(req,res) =>{
-     // NOTE - Update a user
-     return res.json({"message": "User Updated"})
+app.patch('/api/users/:id', (req, res) => {
+
+     const id = Number(req.params.id);
+     const body = req.body;
+     console.log("Received body:", body);
+     const userIndex = users.findIndex(user => user.id === id);
+     if (userIndex === -1) {
+          return res.status(404).json({ "message": "User Not Found" })
+     }
+     users[userIndex] = { ...users[userIndex], ...body };
+     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+          return res.json({ "message": "User Updated" })
+     });
 })
-app.delete('/api/users/:id',(req,res) =>{
+app.delete('/api/users/:id', (req, res) => {
      // NOTE - Delete a user
-     return res.json({"message": "User Deleted"})
+     const id = Number(req.params.id);
+     const userIndex = users.findIndex(user => user.id === id);
+     if (userIndex === -1) {
+          return res.status(404).json({ "message": "User Not Found" })
+     }
+     users.splice(userIndex, 1);
+     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+          return res.json({ "message": "User Deleted" })
+     });
+     return res.json({ "message": "User Deleted" })
 })
 
 
 // here we have 3 same routes but with different path, so the server will know which one to call based on the path and the method.
-app.route('/api/users/:id').get((req,res) => {
-     const id = Number(req.params.id);
-     const user = users.find(user => user.id === id);
-     return res.json(user);
-}).patch((req,res) => {
-     // NOTE - Update a user
-     return res.json({"message": "User Updated"})
-}).delete((req,res) => {
-     // NOTE - Delete a user
-     return res.json({"message": "User Deleted"})
-}) 
+// app.route('/api/users/:id').get((req, res) => {
+//      const id = Number(req.params.id);
+//      const user = users.find(user => user.id === id);
+//      return res.json(user);
+// }).patch((req, res) => {
+//      // NOTE - Update a user
+//      return res.json({ "message": "User Updated" })
+// }).delete((req, res) => {
+//      // NOTE - Delete a user
+//      return res.json({ "message": "User Deleted" })
+// })
 
-app.listen(Port, ()=>{
-
+app.listen(Port, () => {
      console.log(`Server Started at PORT ${Port}`)
 })
+
+
+
+// here i used postman to test the api, you can also use curl or any other tool to test the api. you can also use the browser to test the get requests.
